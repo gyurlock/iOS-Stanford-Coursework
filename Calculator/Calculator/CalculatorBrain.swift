b1825fad68fb9ee123e3b4724ad33d7c08c78456
Gyurme <gyurme+git@gmail.com>
2017-09-07 13:27:54 +1200

    Lecture 2 Making the controller use enums and function types.

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -8,23 +8,79 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
+func multiply(first: Double, second: Double) -> Double {
+    return first * second
+}
+
 struct CalculatorBrain {
     
     private var accumulator: Double?
     
-    mutating func performOperation(_ symbol: String) {
-        
-        switch symbol {
-        case "π" :
-            accumulator = Double.pi
-            break;
-        case "√" :
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
-            }
+    
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations : Dictionary<String, Operation> =
+        [
+            "π" : Operation.constant(Double.pi),
+            "e" : Operation.constant(M_E),
+            "√" : Operation.unaryOperation(sqrt),
+            "cos" : Operation.unaryOperation(cos),
+            "±": Operation.unaryOperation({-$0}),
+            "×": Operation.binaryOperation({$0 * $1}),
+            "÷": Operation.binaryOperation({$0 / $1}),
+            "+": Operation.binaryOperation({$0 + $1}),
+            "-": Operation.binaryOperation({$0 - $1}),
+            "=": Operation.equals
             
-        default:
-            break
+    ]
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value) :
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function) :
+                
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+                break
+            case .equals:
+                performPendingBinaryOperation()
+                break
+            }
+        }
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+   mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+    }
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
         }
     }
     
